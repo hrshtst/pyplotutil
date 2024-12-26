166265c156142f834c28eb30b48a1b21ce2ff212
Hiroshi Atsuta <atsuta@ieee.org>
2024-12-26 12:12:43 +0900

    feat: Add polars based data manipulation

--- src/pyplotutil/_typing.py
+++ src/pyplotutil/_typing.py
@@ -1,18 +1,27 @@
 from __future__ import annotations
 
+from collections.abc import Sequence
 from enum import Enum, auto
 from io import StringIO
 from pathlib import Path
 from typing import Any, Final, Literal, TypeAlias, TypeVar
 
 import numpy as np
-import pandas as pd
+import polars as pl
 
 Unknown: TypeAlias = Any
 FilePath: TypeAlias = str | Path
-DataSourceType: TypeAlias = FilePath | StringIO | pd.DataFrame | pd.Series
+DataSourceType: TypeAlias = FilePath | StringIO | pl.DataFrame | pl.Series
 NumericType: TypeAlias = int | float | complex | np.number
 NumericTypeVar = TypeVar("NumericTypeVar", bound=NumericType)
+
+SingleIndexSelector: TypeAlias = int
+MultiIndexSelector: TypeAlias = slice | range | Sequence[int] | pl.Series | np.ndarray
+SingleNameSelector: TypeAlias = str
+MultiNameSelector: TypeAlias = slice | Sequence[str] | pl.Series | np.ndarray
+BooleanMask: TypeAlias = Sequence[bool] | pl.Series | np.ndarray
+SingleColSelector: TypeAlias = SingleIndexSelector | SingleNameSelector
+MultiColSelector: TypeAlias = MultiIndexSelector | MultiNameSelector | BooleanMask
 
 
 class _NoDefault(Enum):
