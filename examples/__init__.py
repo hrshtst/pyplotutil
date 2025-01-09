0b3618c00fc57c0552c5e5a83df2f3cb8d9bca50
Hiroshi Atsuta <atsuta@ieee.org>
2025-01-09 11:32:29 +0900

    fix: Fix example script

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,1 +1,3 @@
-"""__init__.py"""
+"""Example scripts."""
+
+from __future__ import annotations
--- examples/example.py
+++ examples/example.py
@@ -1,12 +1,18 @@
+"""Example script of pyplotutil module."""
+
 from __future__ import annotations
 
 import argparse
 
+import matplotlib.pyplot as plt
+
 from pyplotutil.datautil import Data
 from pyplotutil.loggingutil import evlog, start_logging
+from pyplotutil.plotutil import apply_ieee_style
 
 
 def parse() -> argparse.Namespace:
+    """Parse provided command arguments."""
     parser = argparse.ArgumentParser(description="Compare scores across preview/delay steps")
     parser.add_argument("data", help="CSV file.")
     parser.add_argument("-o", "--output-directory", dest="output_dir", default=".", help="Output directory")
@@ -21,6 +27,7 @@ def parse() -> argparse.Namespace:
 
 
 def main() -> None:
+    """Run main function."""
     import sys
 
     args = parse()
@@ -32,10 +39,14 @@ def main() -> None:
     data = Data(args.data)
     evlog().info("Loaded data file: %s", args.data)
 
-    evlog().warning("This is WARNING message")
-    evlog().error("This is ERROR message")
-    evlog().critical("This is CRITICAL message")
+    apply_ieee_style()
+    plt.plot(data[:, 0], data[:, 1])
+    plt.show()
 
 
 if __name__ == "__main__":
     main()
+
+# Local Variables:
+# jinx-local-words: "pyplotutil"
+# End:
